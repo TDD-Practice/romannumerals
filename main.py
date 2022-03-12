1d86fbaedbb7039a75cf3996123d57e510c475aa
marcosantonastasi <marco.santonastasi@gmail.com>
2022-03-12 14:40:45 +0100

    refactored fromromans to be algorithmic

--- main.py
+++ main.py
@@ -1,84 +1,37 @@
 def toroman(number):
-    if number == 1:
-        return 'I'
-    if number == 2:
-        return 'II'
-    if number == 3:
-        return 'III'
-    if number == 4:
-        return 'IV'
-    if number == 5:
-        return 'V'
-    if number == 6:
-        return 'VI'
-    if number == 7:
-        return 'VII'
-    if number == 8:
-        return 'VIII'
-    if number == 9:
-        return 'IX'
-    if number == 10:
-        return 'X'
-    if number == 11:
-        return 'XI'
-    if number == 12:
-        return 'XII'
-    if number == 13:
-        return 'XIII'
-    if number == 14:
-        return 'XIV'
-    if number == 15:
-        return 'XV'
-    if number == 16:
-        return 'XVI'
-    if number == 17:
-        return 'XVII'
-    if number == 18:
-        return 'XVIII'
-    if number == 19:
-        return 'XIX'
-    if number == 20:
-        return 'XX'
-    
+    table = {1: 'I',2: 'II',3: 'III',4: 'IV',5: 'V',6: 'VI',7: 'VII',8: 'VIII',9: 'IX',10: 'X'}
+    if number < 10:
+        return table[number]
+    if number % 10 == 0:
+        return 'X' * (number//10)
+    if number < 20:
+        return 'X' + table[number%10]
+
 
 def fromroman(roman):
-    if roman == 'I':
-        return 1
-    if roman == 'II':
-        return 2
-    if roman == 'III':
-        return 3
-    if roman == 'IV':
-        return 4
-    if roman == 'V':
-        return 5
-    if roman == 'VI':
-        return 6
-    if roman == 'VII':
-        return 7
-    if roman == 'VIII':
-        return 8
-    if roman == 'IX':
-        return 9
-    if roman == 'X':
-        return 10
-    if roman == 'XI':
-        return 11
-    if roman == 'XII':
-        return 12
-    if roman == 'XIII':
-        return 13
-    if roman == 'XIV':
-        return 14
-    if roman == 'XV':
-        return 15
-    if roman == 'XVI':
-        return 16
-    if roman == 'XVII':
-        return 17
-    if roman == 'XVIII':
-        return 18
-    if roman == 'XIX':
-        return 19
-    if roman == 'XX':
-        return 20
+    roman = roman[::-1]
+    number = 0
+    i=0
+    while i < len(roman):
+        if roman[i:i+1] == "I":
+            number += 1
+            i+=1
+            if roman[i:i+1] == "I":
+                number += 1
+                i+=1
+            if roman[i:i+1] == "I":
+                number += 1
+                i+=1
+        if roman[i:i+1] == "V":
+            number += 5
+            i+=1
+            if roman[i:i+1] == "I":
+                number -= 1
+                i+=1
+        if roman[i:i+1] == "X":
+            number += 10
+            i+=1
+            if roman[i:i+1] == "I":
+                number -= 1
+                i+=1
+    return number
--- main_test.py
+++ main_test.py
@@ -13,6 +13,16 @@ def test_toroman():
     assert toroman(8) == 'VIII'
     assert toroman(9) == 'IX'
     assert toroman(10) == 'X'
+    assert toroman(11) == 'XI'
+    assert toroman(12) == 'XII'
+    assert toroman(13) == 'XIII'
+    assert toroman(14) == 'XIV'
+    assert toroman(15) == 'XV'
+    assert toroman(16) == 'XVI'
+    assert toroman(17) == 'XVII'
+    assert toroman(18) == 'XVIII'
+    assert toroman(19) == 'XIX'
+    assert toroman(20) == 'XX'
 
 
 def test_fromroman():
@@ -26,3 +36,13 @@ def test_fromroman():
     assert fromroman('VIII') == 8
     assert fromroman('IX') == 9
     assert fromroman('X') == 10
+    assert fromroman('XI') == 11
+    assert fromroman('XII') == 12
+    assert fromroman('XIII') == 13
+    assert fromroman('XIV') == 14
+    assert fromroman('XV') == 15
+    assert fromroman('XVI') == 16
+    assert fromroman('XVII') == 17
+    assert fromroman('XVIII') == 18
+    assert fromroman('XIX') == 19
+    assert fromroman('XX') == 20
