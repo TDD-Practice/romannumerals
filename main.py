30b835168e4fcc2a264ccf2fbb9ab5d3481cff10
marcosantonastasi <marco.santonastasi@gmail.com>
2022-03-12 15:53:05 +0100

    modified to add hundreds in toromans

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 def toroman(number):
     table_units = {0: "", 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX'}
     table_tens = {0: "", 1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'L', 6: 'LX', 7: 'LXX', 8: 'LXXX', 9: 'XC'}
-    roman = ""
+    table_hundreds = {0: "", 1: 'C', 2: 'CC', 3: 'CCC', 4: 'CD', 5: 'D', 6: 'DC', 7: 'DCC', 8: 'DCCC', 9: 'CM'}
     number_units = number % 10
     number_tens = number//10 % 10
+    number_hundreds = number//100 % 10
     
     roman_units = table_units[number_units]
     roman_tens = table_tens[number_tens]
+    roman_hundreds = table_hundreds[number_hundreds]
 
-    return roman_tens + roman_units
+    return roman_hundreds + roman_tens + roman_units
 
 
 def fromroman(roman):
